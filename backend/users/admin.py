a37eafca38c05d2eed76b1643cbcecccc07594ff
ozzimyt <103369868+ozzimyt@users.noreply.github.com>
2023-05-31 02:26:20 +0300

    add User`s admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Follow, User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Администрирование пользователей"""
+    list_display = (
+        'id',
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+    )
+    list_filter = ('username', 'email',)
+    search_fields = ('username', 'email',)
+
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    """Администрирование подписчиков"""
+
+    list_display = ('user', 'author')
+    list_filter = ('user', 'author')
+    search_fields = ('user', 'author')
