96ae3e30f586e05b7275476709dfc692bedc78b8
ozzimyt <103369868+ozzimyt@users.noreply.github.com>
2023-06-01 02:09:34 +0300

    fix requirements, fix project urls, add pagination and permissions in api

---
+++ backend/api/permissions.py
@@ -0,0 +1,13 @@
+from rest_framework.permissions import SAFE_METHODS, IsAuthenticatedOrReadOnly
+
+
+class AuthorOrAdminOrReadOnly(IsAuthenticatedOrReadOnly):
+    """Право доступа только для автора или администратора."""
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in SAFE_METHODS
+            or request.user.is_authenticated
+            and request.user == obj.author
+            or request.user.is_staff
+        )
