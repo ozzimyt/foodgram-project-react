13add22fe7cb8d90daa04a924a45827b391ad1f3
ozzimyt <103369868+ozzimyt@users.noreply.github.com>
2023-05-31 04:24:47 +0300

    add Recipe`s admin

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
+from recipes.models import (FavoriteRecipes, Ingredient, Recipe, ShoppingCart,
+                            Tag, TagInRecipes)
 
-# Register your models here.
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    """ Класс для администирования ингредиентов. """
+
+    list_display = ('id', 'name', 'measurement_unit',)
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    """ Класс для администрирования тегов. """
+
+    list_display = ('id', 'name', 'color', 'slug',)
+    list_filter = ('name', 'slug',)
+    search_fields = ('name',)
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    """ Класс для администрирования рецептов. """
+
+    list_display = ('id', 'name', 'author', 'text', 'cooking_time',
+                    'ingredients', 'tags', 'pub_date')
+    list_filter = ('name', 'author',)
+    search_fields = ('name', 'author', 'cooking_time', 'ingredients', 'tags')
+
+
+@admin.register(TagInRecipes)
+class RecipeTagsAdmin(admin.ModelAdmin):
+    """ Класс для администрирования тегов рецептов. """
+
+    list_display = ('id', 'tag', 'recipe',)
+    list_filter = ('tag', 'recipe',)
+
+
+@admin.register(FavoriteRecipes)
+class FavoriteRecipesAdmin(admin.ModelAdmin):
+    """ Класс для администрирования избранных рецептов. """
+
+    list_display = ('id', 'user', 'recipe',)
+    list_filter = ('user', 'recipe',)
+
+
+@admin.register(ShoppingCart)
+class UserShoppingCartAdmin(admin.ModelAdmin):
+    """ Класс для администрирования пользовательской корзины. """
+
+    list_display = ('id', 'user', 'recipe',)
+    list_filter = ('user', 'recipe',)
